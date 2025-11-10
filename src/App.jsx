18cc8057de319676947435019f7137985043b8b3
strom314 <jakub.gros777@gmail.com>
2025-11-10 20:55:16 +0100

    create a card component that contains the image and a description. Copy a shuffle function from stackoverflow that shuffles an array. Create a displayCards function that will display all the cards according to the ids state

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Card } from "./components/Card";
-import "./app.css"
+import "./app.css";
 
 function App() {
-  /*
-  cards
-  [
-  id:
-  img:
-  clicked:
-  ]
-
-  score
-  highScore
-
-  handleClick(e){
-  id = cards[e.target.id]
-  if(cards[id].clicked){
-    restartGame()
-  }else{
-    cards[id].clicked = true;
-    }
-  }
-*/
   const cardImages = [
     "cei-rigotti.webp",
     "lewisGun.webp",
@@ -45,30 +25,77 @@ function App() {
     "Selbstlader",
   ];
 
-  const [cards, setCards] = useState(
-    cardImages.map((img, idx) => ({
-      clicked: false,
-      id: idx,
-      img: `public/img/${img}`,
-      name: cardNames[idx],
-    }))
-  );
+  const cards = cardImages.map((img, idx) => ({
+    clicked: false,
+    id: idx,
+    img: `public/img/${img}`,
+    name: cardNames[idx],
+  }));
+
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+  function handleClick(id) {
+    const card = cards.find((card) => card.id === id);
+    if (card.clicked) {
+      // gameOver();
+    } else {
+      card.clicked = true;
+    }
+
+    setIds(shuffle(ids));
+  }
+
+  const [ids, setIds] = useState([0, 1, 2, 3, 4, 5, 6, 7]);
+
+  function displayCards() {
+    const cardsToDisplay = ids.map(id => cards.find(card => card.id === id));
+    
+    return cardsToDisplay.map((card) => {
+          return (
+            <Card
+              key={card.id}
+              name={card.name}
+              img={card.img}
+              id={card.id}
+              handleClick={handleClick}
+            ></Card>
+          );
+        })
+  }
+
+  useEffect(() => {
+    displayCards();
+  }, []);
+
   return (
     <>
       <h1>card game</h1>
       <p>score</p>
       <p>high score</p>
       <div className="card-container">
-        {cards.map((card) => {
-          return <Card name={card.name} img={card.img}></Card>;
-        })}
+        {displayCards()}
       </div>
-      {/* cards.map(() => {
-        return <Card img= handleClick= id=>
-      }) 
-       */}
     </>
   );
+}
+
+function shuffle(array) {
+  let currentIndex = array.length;
+  let newArray = array;
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [newArray[currentIndex], newArray[randomIndex]] = [
+      newArray[randomIndex],
+      newArray[currentIndex],
+    ];
+  }
+  return newArray;
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
-export function Card({ img, name }) {
+export function Card({ img, name, id, handleClick }) {
   return (
-    <div className="card">
+    <div  className="card" onClick={() => handleClick(id)}>
       <img src={img} alt={`img of ${name}`} />
       <p>{name}</p>
     </div>
